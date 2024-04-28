b86de2fcf455ba1db768555f6be7a7dc1bd80a91
samuelvergaramartin <sam170703dev@gmail.com>
2024-04-28 11:31:27 +0200

    Cambios

--- src/testgtwindows/TestGtWindows.java
+++ src/testgtwindows/TestGtWindows.java
@@ -15,6 +15,7 @@ public class TestGtWindows {
      */
     public static void main(String[] args) {
         System.out.println("Hello world");
+        System.out.println("Otros cambios otra vez");
     }
     
 }
